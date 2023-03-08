6a150a8b54b68595cb21e9fd4de523cc6d1e0b7b
OtaSteven <sho8517@gmail.com>
2023-03-08 14:34:26 -0800

    Setup the react environement with the folders, edited the App.js since Switch was an outdated version for routing pages

--- src/App.js
+++ src/App.js
@@ -1,61 +1,18 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import EpisodeView from './pages/episodeView/EpisodeView';
-import Video from './pages/video/Video';
-import Login from './pages/login/Login';
-import SignOut from './pages/signOut/SignOut';
-import ExternalLinks from './pages/externalLinks/ExternalLinks';
+import Home from './Pages/Homepage/Home';
+import EpisodeView from './Pages/episodeView/EpisodeView';
 
 
 function App() {
   return (
     <Router>
         <div>
-            <nav>
-                <li>
-                    <Link to="/EpisodeView">EpisodeView</Link>
-                </li>
-                <li>
-                    <Link to="/Video">Video</Link>
-                </li>
-                <li>
-                    <Link to="/Login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/SignOut">SignOut</Link>
-                </li>
-                <li>
-                    <Link to="/ExternalLinks">ExternalLinks</Link>
-                </li>
-            </nav>
-
-            <Switch>
-                <Route path="/EpisodeView">
-                
-                </Route>
-
-                <Route path="/Video">
-
-                </Route>
-
-                <Route path ="/Login">
-
-                </Route>
-
-                <Route path="SignOut">
-
-                </Route>
-
-                <Route path="ExternalLinks">
-
-                </Route>
-            </Switch>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/EpisodeView" element={<EpisodeView />} />
+            </Routes>
         </div>
     </Router>
   );
